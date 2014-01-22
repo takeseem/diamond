201209c5fc083eecf279fb7a0ee233367d9aef80
杨浩 <takeseem@gmail.com>
2014-01-22 12:59:13 +0800

    TO DO - issue JAVA-2: diamond改造 http://10.201.3.70:8888/browse/JAVA-2 log日志

--- diamond-client/src/main/java/com/taobao/diamond/client/impl/DiamondEnv.java
+++ diamond-client/src/main/java/com/taobao/diamond/client/impl/DiamondEnv.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.taobao.diamond.client.BatchHttpResult;
 import com.taobao.diamond.common.Constants;
 import com.taobao.diamond.manager.DiamondManager;
@@ -19,6 +22,7 @@ import com.taobao.diamond.manager.impl.DefaultDiamondManager;
  * @since 0.1.0
  */
 public class DiamondEnv {
+	protected final Log logger = LogFactory.getLog(getClass());
 	private String unitName;
 	private Map<String, DiamondManager> dmMap = new HashMap<String, DiamondManager>();
 	
@@ -37,6 +41,7 @@ public class DiamondEnv {
 	}
 
 	public String getConfig(String dataId, String group, Object gETCONFIG_LOCAL_SNAPSHOT_SERVER, long timeout) throws IOException {
+		if (logger.isDebugEnabled()) logger.debug("dataId=" + dataId + ", " + group + ", " + gETCONFIG_LOCAL_SNAPSHOT_SERVER + ", timeout=" + timeout);
 		DiamondManager dm = getDiamondManager(dataId, group);
 		return dm.getAvailableConfigureInfomation(timeout);
 	}
@@ -54,6 +59,7 @@ public class DiamondEnv {
 	}
 
 	public BatchHttpResult batchQuery(List<String> dataIds, String group, int timeout) {
+		if (logger.isDebugEnabled()) logger.debug("dataId=" + dataIds + ", " + group + ", timeout=" + timeout);
 		BatchHttpResult result = new BatchHttpResult();
 		for (String dataId : dataIds) {
 			try {
